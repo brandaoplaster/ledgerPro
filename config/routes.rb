c08a7b4f7a56a77924aba6ecf291c6f1bf1c0b46
brandaoplaster <brandaoplaster@gmail.com>
2025-12-27 11:55:44 -0300

    feat: Add instruments controller

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
 
   authenticate :user do
     resources :wallets
+    resources :instruments
   end
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
