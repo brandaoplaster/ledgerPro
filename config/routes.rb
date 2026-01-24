61a4909bb7aea9cc0c5e15f9022e405e70512106
brandaoplaster <brandaoplaster@gmail.com>
2026-01-24 16:31:04 -0300

    feat: Add router for strategy rule

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,8 @@ Rails.application.routes.draw do
     end
 
     resources :instruments
+    resources :strategies
+    resources :strategy_rules
   end
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
